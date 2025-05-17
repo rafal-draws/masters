ab42de80d8bca2b8bf254296846ab4ef01b2a8bd
rafal-draws <rafal.w.draws@gmail.com>
2025-05-17 20:11:53 +0200

    pyo3 scrap, exe ciles to be created

--- back/README.md
+++ back/README.md
@@ -2,3 +2,7 @@
 1. Form -> Create session with name and UUID
 2. Upload file -> Save to /songs/UUID-track_num
 3. Show that file
+
+$env:LIBTORCH = "G:\dev\libtorch"
+$env:PATH = "G:\dev\libtorch\lib;" + $env:PATH
+$env:PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
--- back/src/ml.rs
+++ back/src/ml.rs
@@ -1,39 +1,56 @@
 
 
-// pub mod ml {
+pub mod ml {
 
-    
 
-// pub fn add(a: i32, b:i32) -> i32 {
-//     a + b
-// }
+pub fn add(a: i32, b:i32) -> i32 {
+    a + b
+}
 
-//     #[cfg(test)]
-//     mod tests{
+    #[cfg(test)]
+    mod tests{
 
-//         use std::env;
+        use std::env;
 
-//         use tch::{CModule, Tensor};
+        use tch::{CModule, Tensor};
 
-//         use super::*;
-//         #[test]
-//         fn test() {
-//             let model_location = env::var("UPLOADS_DIR").expect("UPLOADS_DIR env var not found");
-//             let model_name = env::var("LATEST_MODEL").expect("LATEST_MODEL env var not found");
-//             let model = CModule::load(format!("{}/{}", model_location, model_name)).expect("model should be loadable");
+        use super::*;
+        fn test_file_load() {
 
-//             let input = Tensor::randn(&[1, 22050], (tch::Kind::Float, tch::Device::Cpu));
-//             let output = model.forward_ts(&[input]).expect("Should produce output");
+            assert!(true)
+        }
 
-//             println!(output);
 
-//             assert(true)
-//         }
+        #[test]
+        fn test() {
+            
+            use dotenv::dotenv;
+            dotenv().ok();
 
-//         #[test]
-//         fn test_add() {
-//             assert_eq!(add(1,2), 3)
-//         }
-//     }
+            let model_location = env::var("UTILS_DIR").expect("UPLOADS_DIR env var not found");
+            let model_name = env::var("LATEST_MODEL").expect("LATEST_MODEL env var not found");
 
-// }
+            let paths = std::fs::read_dir(format!("{}", model_location)).unwrap();
+            for path in paths {
+                println!("{}", path.unwrap().path().display())
+            }
+
+            println!("{}\\{}", model_location, model_name);
+
+            let model = CModule::load(format!("{}\\{}", model_location, model_name)).expect("model should be loadable");
+
+            let input = Tensor::randn(&[1, 1, 22050], (tch::Kind::Float, tch::Device::Cpu));
+            let output = model.forward_ts(&[input]).expect("Should produce output");
+
+            println!("{}", output);
+
+            assert!(true)
+        }
+
+        #[test]
+        fn test_add() {
+            assert_eq!(add(1,2), 3)
+        }
+    }
+
+}
