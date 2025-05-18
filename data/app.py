5ceda3f25465e67b8926433c9ac73a8cc3a77aeb
rafal-draws <rafal.w.draws@gmail.com>
2025-05-18 16:16:31 +0200

    step3 achieved

--- data/app.py
+++ data/app.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
 import os
+import json
+import numpy as np
 
 from typing import Annotated
 from fastapi import FastAPI, HTTPException, Header
+from dotenv import load_dotenv
 
 from server_utils import utilities, transformations
 
@@ -45,8 +47,8 @@ def transform_item(file_name: str):
         raise HTTPException(status_code=500, detail="Internal server error")
     
 
-@app.post("/transform/")
-def transform_item(file_path: Annotated[str | None, Header()] = None,
+@app.post("/transform/step_1")
+def transform_item_step_1(file_path: Annotated[str | None, Header()] = None,
                    filename: Annotated[str | None, Header()] = None):
     
 
@@ -58,20 +60,146 @@ def transform_item(file_path: Annotated[str | None, Header()] = None,
         signal, sr = transformations.extract_signal_for_inference(filename_absolute_path=file_path,
                                                                   length=15,
                                                                   logger=logger)
+        logger.info(f"transformed signal: {signal}, {signal.shape}")
         logger.info(f"{filename} - TRANSFORMED")
+
+        slice_nparray = transformations.generate_npy_array_from_frames(filename=filename,
+                                                                        signal=signal,
+                                                                        artifacts_path=server_data_location,
+                                                                        logger=logger)
+        
+        logger.info(f"slice np array is under {slice_nparray}")
+
         sound_location = transformations.generate_audio_from_frames(filename=filename,
                                                                      signal=signal,
                                                                      sampling_rate=sr,
                                                                      artifacts_path=server_data_location)
         logger.info(f"SOUND FILE LOCATION: {sound_location}")
-
         
 
-        if file_path or filename is None:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return {
 
+        return {
+            "progress": "34%",
+            "sound_location": utilities.normalize_path(sound_location),
+            "signal": utilities.normalize_path(slice_nparray),
+            "sampling_rate": sr,
+            "filename": filename
         }
+        
+    except HTTPException as e:
+        raise e 
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+
+
+
+
+@app.post("/transform/step_2")
+def transform_item_step_2(sound_location: Annotated[str | None, Header()] = None,
+                   signal: Annotated[str | None, Header()] = None,
+                   frame_size: Annotated[str | None, Header()] = None,
+                   hop_size: Annotated[str | None, Header()] = None,
+                   filename: Annotated[str | None, Header()] = None):
+    
+
+    try:
+        sound_location = utilities.normalize_path(sound_location)
+        signal_location = utilities.normalize_path(signal)
+        frame_size = int(frame_size)
+        hop_size = int(hop_size)
+
+        logger.info(sound_location)
+        logger.info(signal)
+
+        with open(signal, 'rb') as f:
+            signal_np = np.load(signal_location)
+
+        logger.info(signal_np.shape)
+
+        frames = transformations.split_signal_to_frame_indexes(frame_size, hop_size, signal_np)
+
+        mels_mp4 = transformations.extract_mels_and_generate_artifacts(
+            frames = frames,
+            frame_size = frame_size,
+            signal = signal_np,
+            filename = filename,
+            artifacts_dir=server_data_location,
+            song_location=sound_location,
+            logger = logger
+        )
+
+        power_mp4 = transformations.extract_power_spectrograms_and_generate_artifacts(
+            frames = frames,
+            frame_size = frame_size,
+            signal = signal_np,
+            filename = filename,
+            artifacts_dir=server_data_location,
+            song_location=sound_location,
+            logger = logger
+        )
+
+        mfcc_mp4 = transformations.extract_mfcc_and_generate_artifacts(
+            frames = frames,
+            frame_size = frame_size,
+            signal = signal_np,
+            filename = filename,
+            artifacts_dir=server_data_location,
+            song_location=sound_location,
+            logger = logger
+        )
+
+
+
+        return {
+            "progress": "45%",
+            "sound_location": sound_location,
+            "signal": signal,
+            "power_mp4": power_mp4,
+            "mel_mp4": mels_mp4,
+            "mfcc_mp4": mfcc_mp4
+        }
+        
+    except HTTPException as e:
+        raise e 
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+
+
+@app.post("/transform/step_3")
+def transform_item_step_3(signal_location: Annotated[str | None, Header()] = None):
+    
+
+    try:
+        signal_location = utilities.normalize_path(signal_location)
+
+        logger.info(signal_location)
+
+        with open(signal_location, 'rb') as f:
+            signal_np = np.load(signal_location)
+
+
+
+        X = []
+
+        
+        for i in range(0,14,1):
+            X.append(signal_np[i*22050:(i+1)*22050].astype(np.float32).tolist())
+            # X.append(json.dumps(signal_np[i*22050:(i+1)*22050]))
+
+
+        response_dict = {index: value for index, value in enumerate(X)}
+
+
+        return {
+            "progress": "76%",
+            "signal": X,
+            "frames": response_dict
+        }
+        
     except HTTPException as e:
         raise e 
     except Exception as e:
--- data/server_utils/transformations.py
+++ data/server_utils/transformations.py
@@ -3,11 +3,32 @@ import librosa as li
 import numpy as np
 import soundfile as sf
 
+import matplotlib.pyplot as plt
+import matplotlib
 
-def generate_audio_from_frames(filename, signal, sampling_rate, artifacts_path):
-    sf.write(f"{artifacts_path}slices/{filename}.wav", signal, sampling_rate)
-    return f"{artifacts_path}slices/{filename}.wav"
+from . import utilities
 
+
+def generate_audio_from_frames(filename: str, signal, sampling_rate, artifacts_path):
+    if filename.endswith(".mp3"):
+        x = filename.replace(".mp3", ".wav")
+    sf.write(utilities.normalize_path(f"{artifacts_path}/slices/{x}"), signal, sampling_rate)
+    return f"{artifacts_path}/slices/{x}"
+
+def generate_npy_array_from_frames(filename: str, signal, artifacts_path, logger):
+
+    logger.info(filename)
+
+    if filename.endswith(".mp3"):
+        x = filename.replace(".mp3", ".npy")
+
+    if filename.endswith(".wav"):
+        x = filename.replace(".wav", ".npy")
+
+    with open(f'{artifacts_path}/transformed_signals/{x}', 'wb') as f:
+        np.save(f, signal)
+
+    return f"{artifacts_path}/transformed_signals/{x}"
 
 
 
@@ -52,6 +73,9 @@ def extract_signal_for_inference(filename_absolute_path, length, logger):
 
             
             logger.info(f"returning {filename_absolute_path} as signal ")
+
+            
+
             return y, sr
         except Exception as e:
             print(f"Error during slicing a record to a minute! {e}")
@@ -95,3 +119,195 @@ def get_hanned(seconds: int, y, sampling_rate: int, debug: bool) -> np.ndarray :
     
     
     return y_mod
+
+
+def extract_mels_and_generate_artifacts(frames, signal: np.array, filename: str, artifacts_dir: str, frame_size: int, song_location: str, logger):
+    logger.info("Extracting mels")
+    mels = extract_mels(frames, signal)
+    logger.info("generating mel images")
+    generate_mel_spec_images(filename, mels, artifacts_dir)
+    logger.info("generating mel video")
+    mp4 = generate_video_from_mel_spec_images(filename, song_location, len(frames), artifacts_dir)
+    return mp4
+
+def extract_power_spectrograms_and_generate_artifacts(frames, signal: np.array, filename: str, artifacts_dir: str, frame_size: int, song_location: str, logger):
+    logger.info("Extracting power spectrograms")
+    power_spectrograms = extract_power_spectrograms(frames, signal)
+    logger.info("Generating power spectrograms images")
+    generate_power_spectr_spec_images(filename, power_spectrograms, artifacts_dir)
+    logger.info("Generating a video")
+    mp4 = generate_video_from_power_spetr_images(filename, song_location, len(frames), artifacts_dir)
+
+    return mp4
+
+def extract_mfcc_and_generate_artifacts(frames, signal: np.array, filename: str, artifacts_dir: str, frame_size: int, song_location: str, logger):
+    logger.info("Extractings mfccs")
+    mfcc = extract_mfccs(frames, signal)
+    logger.info("Generating mfcc images")
+    generate_mfcc_images(filename, mfcc, artifacts_dir)
+    logger.info("Genearting mfcc video")
+    mp4 = generate_video_from_mfcc_images(filename, song_location, len(frames), artifacts_dir)
+    return mp4
+
+
+
+
+def generate_mel_spec_images(filename, song_mels, server_data_path):
+    
+    matplotlib.use('Agg')
+    
+    fmin = 0
+    fmax = 8000
+    vmin = -80  # common min for dB
+    vmax = 0 
+    
+    fig = plt.figure()
+    
+    for index, i in enumerate(song_mels):
+        fig, ax = plt.subplots()
+        S_dB = li.power_to_db(i, ref=np.max)
+        img = li.display.specshow(S_dB,
+                          x_axis='frames',
+                          y_axis='mel',
+                          sr=22050,
+                          fmax=fmax,
+                          ax=ax,
+                          vmin=vmin, vmax=vmax)  # fix color scale
+
+        fig.colorbar(img, ax=ax, format='%+2.0f dB')
+        ax.set(title='Mel-frequency spectrogram')
+        plt.savefig(utilities.normalize_path(f"{server_data_path}/mels/{filename}-{index}.png"))
+        plt.close(fig)
+
+
+def generate_video_from_mel_spec_images(filename, song_filename, frames_length, server_data_path):
+    
+    os.system(
+    # f"ffmpeg -framerate 5.355 -y " 4096 frames 
+    f"ffmpeg -framerate {(frames_length/14):.2f} -y " 
+    f"-i {server_data_path}/mels/{filename}-%d.png "
+    f"-i {song_filename} "
+    # f" -vf minterpolate='fps=10' "  makes clunky
+    f"-c:v libx264 -pix_fmt yuv420p "
+    f"-c:a aac -b:a 192k -shortest "
+    f"{server_data_path}/videos/mel-{filename}.mp4"
+    )
+
+    return f"{server_data_path}/videos/mel-{filename}.mp4"
+
+
+def generate_power_spectr_spec_images(filename, power_spectrograms, server_data_path):
+    vmin = 0
+    vmax = 1
+    
+    
+    for index, i in enumerate(power_spectrograms): ## TODO FIXIT
+        fig = plt.figure()
+
+        li.display.specshow(i, y_axis='chroma', x_axis='time', 
+                          # fmax=fmax,
+                          vmin=vmin, vmax=vmax)
+        plt.colorbar()  # optional, shows scale
+        plt.title("Chroma Power Spectrogram (Short Fourier Transform)")
+        plt.savefig(f"{server_data_path}/power/{filename}-{index}.png")
+        plt.close(fig)
+
+def generate_video_from_power_spetr_images(filename: str, song_filename, frames_length, server_data_path):
+
+    os.system(
+    # f"ffmpeg -framerate 5.355 -y " 4096 frames 
+    f"ffmpeg -framerate {(frames_length/14):.2f} -y " 
+    f"-i {server_data_path}/power/{filename}-%d.png "
+    f"-i {song_filename} "
+    # f" -vf minterpolate='fps=10' "  makes clunky
+    f"-c:v libx264 -pix_fmt yuv420p "
+    f"-c:a aac -b:a 192k -shortest "
+    f"{server_data_path}/videos/power-{filename}.mp4"
+    )
+
+    return f"{server_data_path}/videos/power-{filename}.mp4"
+
+
+
+def generate_mfcc_images(filename, mfccs, server_data_path):
+
+    # global_min = min(mfcc.min() for mfcc in mfccs)
+    # # global_max = max(mfcc.max() for mfcc in mfccs)
+    # global_max = 1000
+    
+    for index, i in enumerate(mfccs): 
+        fig = plt.figure()
+
+        li.display.specshow(i, x_axis='time', 
+                            # vmin=global_min,
+                            # vmax=global_max
+                           )
+        plt.ylim(0, 20)
+        plt.colorbar()  # optional, shows scale
+        plt.title("Mel-Frequency Cepstral Coefficients (MFCCs)")
+        plt.savefig(f"{server_data_path}/mfcc/{filename}-{index}.png")
+        plt.close(fig)
+
+
+def generate_video_from_mfcc_images(filename, song_filename, frames_length, server_data_path):
+    os.system(
+    # f"ffmpeg -framerate 5.355 -y " 4096 frames 
+    f"ffmpeg -framerate {(frames_length/14):.2f} -y " 
+    f"-i {server_data_path}/mfcc/{filename}-%d.png "
+    f"-i {song_filename} "
+    # f" -vf minterpolate='fps=10' "  makes clunky
+    f"-c:v libx264 -pix_fmt yuv420p "
+    f"-c:a aac -b:a 192k -shortest "
+    f"{server_data_path}/videos/mfcc-{filename}.mp4"
+    )
+
+    return f"{server_data_path}/videos/mfcc-{filename}.mp4"
+
+
+def extract_mels(frames_indexes, signal: np.array, sr: int = 22050, fft_window: int = 256, feature_shape: int = 20):
+    mels = []
+    for i in frames_indexes:
+        current_frame = signal[i[0]:i[1]]
+        mels.append(li.feature.melspectrogram(y=current_frame, sr=sr, n_fft=fft_window, n_mels=feature_shape))
+    
+    return np.array(mels)
+
+def extract_power_spectrograms(frames_indexes, signal: np.array, sr: int = 22050, feature_shape: int = 20):
+    power_spectograms = []
+    for i in frames_indexes:
+        current_frame = signal[i[0]:i[1]]
+        power_spectograms.append(li.feature.chroma_stft(y=current_frame, sr=sr, n_chroma=feature_shape))
+    
+    return np.array(power_spectograms)
+
+def extract_mfccs(frames_indexes, signal: np.array, sr: int = 22050, feature_shape: int = 20):
+    mels = []
+    for i in frames_indexes:
+        current_frame = signal[i[0]:i[1]]
+        mels.append(li.feature.mfcc(y=current_frame, sr=22050, n_mfcc=feature_shape))
+    
+    return np.array(mels)
+
+
+def split_signal_to_frame_indexes(frame_size: int, hop_size: int, signal: np.array):
+    """
+    takes in frame size, hop size, signal (1 dim array of floats)
+
+    returns list of tuples of frame indexes of (frame_size - hop_size, frame_size)
+    """
+    try:
+        frame_count = signal.shape[0] // frame_size 
+        
+        
+        frames = []
+        for i in range (0, frame_count):
+            if i == 0:
+                start, stop = 0, frame_size + hop_size
+            else:
+                start, stop = (i*frame_size - hop_size, (i+1)*frame_size)
+        
+            frames.append((start,stop))
+        return frames
+    except Exception as e:
+        print(f"Error extracting frames: {e}")
+        return np.nan
