abc4dbd78a4ced1c8aedff918148e38fefbd5764
rafal-draws <rafal.w.draws@gmail.com>
2025-07-13 20:32:04 +0200

    chrust address fixes + todo server_data shared loc

--- back/src/main.rs
+++ back/src/main.rs
@@ -57,7 +57,7 @@ async fn main() -> Result<(), Box<std::io::Error>> {
         .layer(config::create_cors_layers())
         .layer(DefaultBodyLimit::max(250 * 1024 * 1024));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("listening on http://{:?}/", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
